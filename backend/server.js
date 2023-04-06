0d6e609e126df0e23613cf95be76a4a0505f3514
Farid <0muhammadfarid@gmail.com>
2023-04-06 10:42:17 +0800

    move files

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 let dbConnect = require("./dbConnect");
+const cors = require("cors");
 
 // parse requests of content-type -application/json
 
 app.use(express.json());
-
+app.use(cors());
 //app.get("/", (req, res) => {
 //    res.json({ message: "Welcome to my  application." });
 //});
 
 let userRoutes = require('./routes/userRoutes')
-app.use('/api/users', userRoutes)
+app.use('./api/users', userRoutes)
 
 // set port, listen for requests
 const PORT = process.env.DB_PORT || 1000;
